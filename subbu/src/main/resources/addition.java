a8e69fcbc735f517e22f9139286158c1d325e890
ex90 <ex90@RP21SYS28.win2k18.local>
2023-12-21 10:18:19 +0530

    exam

---
+++ subbu/src/main/resources/addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class addition {
+	public static void main(String[] args) {
+	    int x, y, sum;
+	    Scanner Obj = new Scanner(System.in);
+	    System.out.println("Type a number:");
+	    x = Obj.nextInt();
+
+	    System.out.println("Type another number:");
+	    y = Obj.nextInt();
+
+	    add = x+y;
+	    System.out.println("addition is: " + add);
+	  }
+
+}
+
+
---
+++ subbu/src/main/resources/subtraction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class subtraction {
+	int x, y, sum;
+    Scanner Obj = new Scanner(System.in);
+    System.out.println("Type a number:");
+    x = Obj.nextInt();
+
+    System.out.println("Type another number:");
+    y = Obj.nextInt();
+
+    sub = x+y;
+    System.out.println("subtraction is: " + sub);
+  }
+
+}
